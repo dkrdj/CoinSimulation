b4de86f595f8126a6c05bf72527b721efc301bc0
dkrdj <dkrdj1000@naver.com>
2023-09-28 00:50:07 +0900

    before building, test API

---
+++ src/main/java/com/coinsimulation/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.coinsimulation.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+    @GetMapping("1")
+    public Flux<String> test() {
+        return Flux.interval(Duration.ofMillis(300L))
+                .map(tick -> "test" + tick)
+                .take(10);
+    }
+}

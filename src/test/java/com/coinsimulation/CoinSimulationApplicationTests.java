a88cb0c7092d54e63d220a57bb65157643ecb2c7
dkrdj <dkrdj1000@naver.com>
2023-10-04 00:36:53 +0900

    test fix

--- src/test/java/com/coinsimulation/CoinSimulationApplicationTests.java
+++ src/test/java/com/coinsimulation/CoinSimulationApplicationTests.java
@@ -1,9 +1,8 @@
 package com.coinsimulation;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 //@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CoinSimulationApplicationTests {
 

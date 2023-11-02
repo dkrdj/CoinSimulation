1fa4994d8884d1ba90222a25ec674f9f2bd7d21b
dkrdj <dkrdj1000@naver.com>
2023-11-03 02:02:15 +0900

    pull build

--- src/main/java/com/coinsimulation/controller/OrderBookController.java
+++ src/main/java/com/coinsimulation/controller/OrderBookController.java
@@ -21,6 +21,7 @@ public class OrderBookController {
         return this.orderBookService.subscribeOrderBook(code)
                 .map(ResponseEntity::ok);
     }
+    //
 
     @MessageExceptionHandler(IllegalArgumentException.class)
     public Mono<ResponseEntity<String>> handleException(Exception e) {
